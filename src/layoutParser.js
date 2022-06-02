b290347568bb38a8abeb1744117db4a3fed39b4b
Ryan Martin <eqpoqpe@gmail.com>
2022-06-02 00:19:02 +0000

    upload

--- src/layoutParser.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.layoutParser = void 0;
-function layoutParser() { }
-exports.layoutParser = layoutParser;
--- src/le-render.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.render = void 0;
-var layoutParser_1 = require("./layoutParser");
-function render() {
-    (0, layoutParser_1.layoutParser)();
-}
-exports.render = render;
